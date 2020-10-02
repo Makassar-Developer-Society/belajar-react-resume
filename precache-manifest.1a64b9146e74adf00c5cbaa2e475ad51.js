caea2e3089ec757f1311428b92db88c2ecec0c1d
alsanusi <malkautsars@gmail.com>
2020-10-02 18:56:49 +0800

    Updates

---
+++ precache-manifest.1a64b9146e74adf00c5cbaa2e475ad51.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46cce40157bddc98a56c10f6928403db",
+    "url": "/belajar-react-resume/index.html"
+  },
+  {
+    "revision": "e603f24a2e3e9c156a29",
+    "url": "/belajar-react-resume/static/css/main.65419d3e.chunk.css"
+  },
+  {
+    "revision": "75ca59d10aa01e816627",
+    "url": "/belajar-react-resume/static/js/2.b3314231.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/belajar-react-resume/static/js/2.b3314231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e603f24a2e3e9c156a29",
+    "url": "/belajar-react-resume/static/js/main.b301e1ae.chunk.js"
+  },
+  {
+    "revision": "3b31439e14d98b220f37",
+    "url": "/belajar-react-resume/static/js/runtime-main.46ef18c0.js"
+  },
+  {
+    "revision": "7802d8b27fcb19893ce6b38c0789268e",
+    "url": "/belajar-react-resume/static/media/Raleway-Bold.7802d8b2.ttf"
+  },
+  {
+    "revision": "6c084270ccdeb72fd9f5a5144cea628f",
+    "url": "/belajar-react-resume/static/media/Raleway-Light.6c084270.ttf"
+  },
+  {
+    "revision": "53e639e09466905cd0944970529ad50f",
+    "url": "/belajar-react-resume/static/media/me.53e639e0.jpeg"
+  }
+]);
